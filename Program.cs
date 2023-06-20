1ccb01ddcb57f6f08fd519573cecb2fbf83cd620
Alexander Grigoriev <nomad1995@yandex.ru>
2023-06-20 22:37:47 +0300

    f

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿/* Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет,
+является ли оно палиндромом. */
+
+// int n,r,sum=0,temp;    
+// Console.WriteLine("Enter the number: ");   
+// n = Convert.ToInt32(Console.ReadLine());  
+// temp=n;      
+// while(n>0)      
+// {      
+// r=n%10;      
+// sum=(sum*10)+r;      
+// n=n/10;      
+// }      
+// if(temp==sum)      
+// Console.WriteLine("Number is a palindrome.");      
+// else      
+// Console.WriteLine("Number is not a palindrome");     
+ 
+/*Задача 21
+Напишите программу, которая принимает на вход координаты двух точек 
+и находит расстояние между ними в 3D пространстве */
+// double x1, y1, z1, x2, y2, z2, d;
+
+// Console.WriteLine("Enter the number: ");   
+// x1 = Convert.ToDouble(Console.ReadLine());
+
+// Console.WriteLine("Enter the number: ");   
+// y1 = Convert.ToDouble(Console.ReadLine());
+
+// Console.WriteLine("Enter the number: ");   
+// z1 = Convert.ToDouble(Console.ReadLine());
+
+// Console.WriteLine("Enter the number: ");   
+// x2 = Convert.ToDouble(Console.ReadLine());
+
+// Console.WriteLine("Enter the number: ");   
+// y2 = Convert.ToDouble(Console.ReadLine());
+
+// Console.WriteLine("Enter the number: ");   
+// z2 = Convert.ToDouble(Console.ReadLine());
+
+// d = Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1));
+// Console.WriteLine(d);
+
+/* Задача 23
+Напишите программу, которая принимает на вход число (N)
+и выдаёт таблицу кубов чисел от 1 до N. */
+
+// Console.WriteLine("Enter number:");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int cube, i = 1;
+// while (i <= n)
+// {
+//     cube = i*i*i;
+    
+//     Console.WriteLine("{0}", cube);
+
+//     i++; 
+// }
+
